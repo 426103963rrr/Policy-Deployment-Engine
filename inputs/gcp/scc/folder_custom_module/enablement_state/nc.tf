4f099735898a14cf9e0003f17380d82d19a8f62a
Saifmalikk <safymalik@yahoo.com>
2025-09-15 18:47:14 +1000

    made some required changes

--- inputs/gcp/scc/folder_custom_module/enablement_state/nc.tf
+++ inputs/gcp/scc/folder_custom_module/enablement_state/nc.tf
@@ -1,41 +1,75 @@
-# Describe your resource type here
-# Keep "nc" as the name to indicate that this resource and its attributes are non-compliant
-
-resource "google_folder" "folder_nc" {
-  parent       = "organizations/123456789"
-  display_name = "folder-name"
+resource "google_folder" "folder_nc1" {
+  parent              = "organizations/123456789"
+  display_name        = "nc1"
   deletion_protection = false
 }
 
-resource "google_scc_folder_custom_module" "nc" {
-  folder = google_folder.folder_nc.folder_id
-  display_name = "enablement_state"
+resource "google_scc_folder_custom_module" "nc1" {
+  folder           = google_folder.folder_nc1.folder_id
+  display_name     = "nc1"
   enablement_state = "DISABLED"
+
   custom_config {
     predicate {
-      expression = "resource.rotationPeriod > duration(\"2592000s\")"
-      title = "Purpose of the expression"
-      description = "description of the expression"
-      location = "location of the expression"
+      expression   = "resource.rotationPeriod > duration(\"2592000s\")"
+      title        = "Purpose of the expression"
+      description  = "description of the expression"
+      location     = "location of the expression"
     }
     custom_output {
       properties {
         name = "duration"
         value_expression {
-          expression = "resource.rotationPeriod"
-          title = "Purpose of the expression"
-          description = "description of the expression"
-          location = "location of the expression"
+          expression   = "resource.rotationPeriod"
+          title        = "Purpose of the expression"
+          description  = "description of the expression"
+          location     = "location of the expression"
         }
       }
     }
     resource_selector {
-      resource_types = [
-        "cloudkms.googleapis.com/CryptoKey",
-      ]
+      resource_types = ["cloudkms.googleapis.com/CryptoKey"]
     }
-    severity = "LOW"
-    description = "Description of the custom module"
+    severity       = "LOW"
+    description    = "Description of the custom module"
+    recommendation = "Steps to resolve violation"
+  }
+}
+
+resource "google_folder" "folder_nc2" {
+  parent              = "organizations/123456789"
+  display_name        = "nc2"
+  deletion_protection = false
+}
+
+resource "google_scc_folder_custom_module" "nc2" {
+  folder           = google_folder.folder_nc2.folder_id
+  display_name     = "nc2"
+  enablement_state = "DISABLED"
+
+  custom_config {
+    predicate {
+      expression   = "resource.rotationPeriod > duration(\"2592000s\")"
+      title        = "Purpose of the expression"
+      description  = "description of the expression"
+      location     = "location of the expression"
+    }
+    custom_output {
+      properties {
+        name = "duration"
+        value_expression {
+          expression   = "resource.rotationPeriod"
+          title        = "Purpose of the expression"
+          description  = "description of the expression"
+          location     = "location of the expression"
+        }
+      }
+    }
+    resource_selector {
+      resource_types = ["cloudkms.googleapis.com/CryptoKey"]
+    }
+    severity       = "LOW"
+    description    = "Description of the custom module"
     recommendation = "Steps to resolve violation"
   }
 }
