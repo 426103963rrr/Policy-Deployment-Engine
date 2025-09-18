68b5b445a6f6cc13ccf7de1edd81f0b16d60e2e6
Saifmalikk <safymalik@yahoo.com>
2025-09-18 19:38:17 +1000

    Resolved merge conflicts in event threate detection  and folder custom module resources and plans

--- inputs/gcp/scc/event_threat_detection_custom_module/enablement_state/nc.tf
+++ inputs/gcp/scc/event_threat_detection_custom_module/enablement_state/nc.tf
@@ -1,25 +1,7 @@
 resource "google_scc_event_threat_detection_custom_module" "nc" {
-<<<<<<< HEAD
   display_name     = "nc"
   type             = "CONFIGURABLE_BAD_IP"
   organization     = "123456789"
-=======
-    display_name= "nc"
-    type = "CONFIGURABLE_BAD_IP"
-    organization = "123456789"
-    config = jsonencode({
-        "metadata": {
-            "severity": "LOW",
-            "description": "Flagged by Forcepoint as malicious",
-            "recommendation": "Contact the owner of the relevant project."
-            },
-        "ips": [
-            "192.0.2.1",
-            "192.0.2.0/24"
-        ]
-    })
-    enablement_state = "DISABLED"
->>>>>>> dev
 
   config = jsonencode({
     "metadata": {
